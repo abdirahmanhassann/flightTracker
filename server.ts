b3fc0838a2636575fbf8a046548bd4c6a32f2e75
unknown <abdi12340.am@gmail.com>
2023-05-11 20:16:13 +0300

    Search feature and click button

--- server.ts
+++ server.ts
@@ -29,7 +29,8 @@
       }
     
       // Fill in origin and destination
-      
+      await new Promise(resolve => setTimeout(resolve, 1000));
+
       const input = await page.$x('//*[@id="i15"]/div[1]/div/div/div[1]/div/div/input');
       await input[0].click();
       await page.keyboard.press('End'); // move cursor to the end of the input
@@ -40,10 +41,10 @@
       await page.keyboard.up('Shift'); // release shift key
       await page.keyboard.type('london')
       await page.keyboard.press('ArrowRight')
-      await page.keyboard.press('Enter')
+      await page.keyboard.press('Enter')    
 
-      
       const input2= await page.$x('//*[@id="i15"]/div[4]/div/div/div[1]/div/div/input')
+      
       await input2[0].click();
       await page.keyboard.press('End'); // move cursor to the end of the input
       await page.keyboard.down('Shift'); // hold shift key
@@ -51,10 +52,9 @@
           await page.keyboard.press('Backspace');
       }
       await page.keyboard.up('Shift'); // release shift key
-      await page.keyboard.type('istanbul')
+      await page.keyboard.type('dublin')
       await page.keyboard.press('ArrowRight')
-      await page.keyboard.press('Enter')      
-      await new Promise(resolve => setTimeout(resolve, 500000));
+      await page.keyboard.press('Enter')    
       await page.$eval('#yDmH0d > c-wiz.zQTmif.SSPGKf > div > div:nth-child(2) > c-wiz > div.cKvRXe > c-wiz > div.vg4Z0e > div:nth-child(1) > div.SS6Dqf.POQx1c > div.AJxgH > div > div.rIZzse > div.bgJkKe.K0Tsu > div > div > div.cQnuXe.k0gFV > div > div > div:nth-child(1) > div > div.oSuIZ.YICvqf.kStSsc.ieVaIb > div > input',
       (el:any, value:any) => el.value = value, '25/07/2023')
       await page.$eval('#yDmH0d > c-wiz.zQTmif.SSPGKf > div > div:nth-child(2) > c-wiz > div.cKvRXe > c-wiz > div.vg4Z0e > div:nth-child(1) > div.SS6Dqf.POQx1c > div.AJxgH > div > div.rIZzse > div.bgJkKe.K0Tsu > div > div > div.cQnuXe.k0gFV > div > div > div:nth-child(1) > div > div.oSuIZ.YICvqf.lJODHb.qXDC9e > div > input',
@@ -64,10 +64,11 @@
       
       // Submit the form 
       
+      
+      await new Promise(resolve => setTimeout(resolve, 2000));
+      const submitButton = await page.$x('//*[@id="yDmH0d"]/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[2]/div/button');
+      await submitButton[0].click();
       await new Promise(resolve => setTimeout(resolve, 500000));
-
-  const submitButton = await page.$('#yDmH0d > c-wiz.zQTmif.SSPGKf > div > div:nth-child(2) > c-wiz > div.cKvRXe > c-wiz > div.vg4Z0e > div:nth-child(1) > div.SS6Dqf.POQx1c > div.MXvFbd > div > button');
-  await submitButton.click();
   await page.waitForNavigation()
       // Wait for search results to load
       await page.waitForSelector('.gws-flights-results__itinerary-card-summary', { visible: true });
